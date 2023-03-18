afb11c0e0d406fce20bdfb28a22ad0b9f8ecab01
Shivanand <Shivanand0297@gmail.com>
2023-03-18 20:01:24 +0530

    add movie into the database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Register from "./components/Register";
 // db from firestore
 import { db } from "./config/firebaseConfig";
 
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, addDoc, collection } from "firebase/firestore";
+
+// toast imports
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const [movieList, setMovieList] = useState([]);
@@ -12,6 +15,14 @@ const App = () => {
   // first getting collection ref to get the movies lists
   const moviesCollection = collection(db, "movies");
 
+  // states for adding movies
+  const [movieDetails, setMovieDetails] = useState({
+    title: "",
+    releaseDate: "",
+    isReceivedAnOscar: false
+  })
+
+  // fetching all the movies from firestore db
   const getMovies = async () => {
     // READ THE DATA FROM FIRESTORE
     // SET THE DATA movieList
@@ -36,9 +47,80 @@ const App = () => {
     getMovies();
   }, []);
 
+// change function
+  const handleChange = (item, e) =>{
+
+    // changing details on input name
+    if(item === "text" || item === "number"){
+      setMovieDetails((prev)=>({
+        ...prev, [e.target.name]: e.target.value
+      }))
+    }else{
+      setMovieDetails((prev)=>({
+        ...prev, [e.target.name]: e.target.checked
+      }))
+    }
+  }
+
+  // function to add movie into the firebase database
+  const addMovie = async () =>{
+    try {
+
+      // first getting collection reference and then adding data obj
+      await addDoc(moviesCollection, {
+        title: movieDetails.title,
+        releaseDate: movieDetails.releaseDate,
+        receivedAnOscar: movieDetails.isReceivedAnOscar
+      })
+
+      // success toast
+      toast.success("Movie Added Successfully", {
+        position: "top-center",
+      })
+
+      // reseting the details to default
+      setMovieDetails({
+        title: "",
+        releaseDate: "",
+        isReceivedAnOscar: false
+
+      })
+
+      getMovies();
+      
+    } catch (err) {
+      console.log(err)
+      toast.error("Could not add movie")
+    }
+  }
+
   return (
     <div>
       <Register />
+      <div>
+        <input 
+          onChange={(e)=> {handleChange("text", e)}}
+          type="text"
+          name="title"
+          placeholder="Movie title"
+          value={movieDetails.title}
+        />
+        <input 
+          onChange={(e)=> {handleChange("number", e)}}
+          type="number"
+          name="releaseDate"
+          placeholder="ReleaseDate"
+          value={movieDetails.releaseDate ? Number(movieDetails.releaseDate) : "" }
+        />
+        <input 
+          onChange={(e)=>{handleChange("checkbox", e)}}
+          type="checkbox"
+          name="isReceivedAnOscar"
+          value={Boolean(movieDetails.isReceivedAnOscar)}
+        />
+        <label>Received an Oscar</label>
+        <button onClick={addMovie}>Add Movie</button>
+      </div>
       { movieList.map(movie=>(
         <div key={movie.id}>
           <h2 style={{ color: movie.receivedAnOscar ? "green" : "red" }} >Title: {movie.title}</h2>
@@ -47,6 +129,7 @@ const App = () => {
           
         </div>
       ))}
+      <Toaster/>
     </div>
   );
 };
